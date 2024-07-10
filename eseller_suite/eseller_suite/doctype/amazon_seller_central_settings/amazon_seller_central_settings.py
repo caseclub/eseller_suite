c6a2a6632e8b6af4b4aec48f29075eed6bb73a25
SherinKR <sherinkrply@gmail.com>
2024-07-10 14:47:48 +0530

    feat: Fetching Orders with limit

--- eseller_suite/eseller_suite/doctype/amazon_seller_central_settings/amazon_seller_central_settings.py
+++ eseller_suite/eseller_suite/doctype/amazon_seller_central_settings/amazon_seller_central_settings.py
@@ -5,17 +5,19 @@ import frappe
 import requests
 import json
 from datetime import datetime
-from frappe.utils import get_datetime, add_to_date
+from frappe.utils import add_days, today, get_link_to_form
 from frappe.model.document import Document
-DATE_FORMAT = "%Y-%m-%d"
-TIME_FORMAT = "%H:%M:%S.%f"
-DATETIME_FORMAT = f"{DATE_FORMAT} {TIME_FORMAT}"
-
+from frappe import _
 
 class AmazonSellerCentralSettings(Document):
-	def on_update(self):
-		if self.enable:
-			generate_access_token()
+     def validate(self):
+           self.validate_after_date()
+
+     def validate_after_date(self):
+        if datetime.strptime(add_days(today(), -30), "%Y-%m-%d") > datetime.strptime(
+            self.after_date, "%Y-%m-%d"
+        ):
+            frappe.throw(_("The date must be within the last 30 days."))
 
 def get_authorisation_token():
 	'''
@@ -25,20 +27,7 @@ def get_authorisation_token():
 		"Amazon Seller Central Settings", "Amazon Seller Central Settings", "authorisation_token"
 	)
 
-
 def get_access_token():
-	'''
-		Method to get access_token from the Settings, if exprired generate and return new token
-	'''
-	if frappe.db.get_single_value("Amazon Seller Central Settings", "access_token"):
-		token_exprire_on = frappe.db.get_single_value("Amazon Seller Central Settings", "token_exprire_on")
-		if token_exprire_on and get_datetime()> get_datetime(token_exprire_on):
-			return frappe.utils.password.get_decrypted_password(
-				"Amazon Seller Central Settings", "Amazon Seller Central Settings", "access_token"
-			)
-	return generate_access_token()
-
-def generate_access_token():
 	'''
 		Method to generate access_token
 	'''
@@ -63,25 +52,21 @@ def generate_access_token():
 	)
 	if response.ok:
 		response_json = response.json()
-		expires_in = 3600
-		if response_json.get('expires_in'):
-			expires_in = response_json.get('expires_in')
 		if response_json.get('access_token'):
 			access_token = response_json.get('access_token')
-			token_generated_on = get_datetime()
-			token_exprire_on = add_to_date(token_generated_on, seconds=expires_in)
-			frappe.db.set_single_value("Amazon Seller Central Settings", "access_token", access_token)
-			frappe.db.set_single_value("Amazon Seller Central Settings", "token_generated_on", get_datetime(token_generated_on).strftime(DATETIME_FORMAT))
-			frappe.db.set_single_value("Amazon Seller Central Settings", "token_exprire_on", get_datetime(token_exprire_on).strftime(DATETIME_FORMAT))
 			return access_token
 
 @frappe.whitelist()
-def get_orders(docname):
+def get_orders(max_results=10, created_after=None, next_token=None):
+    if not created_after:
+        created_after = today()
     token = get_access_token()
     url = "https://sellingpartnerapi-eu.amazon.com/orders/v0/orders"
     response_data = {
         "MarketplaceIds": "A21TJRUUN4KGV",
-        "CreatedAfter": "2024-06-25"
+        "MaxResultsPerPage": max_results,
+        "CreatedAfter": created_after,
+        "NextToken": next_token
     }
 
     headers = {
@@ -90,17 +75,22 @@ def get_orders(docname):
     }
 
     response = requests.get(url, headers=headers, params=response_data)
+    response_json = response.json()
+    print("response_json ", response_json)
     if response.ok:
-        response_json = response.json()
         if response_json.get('payload'):
             payload = response_json.get('payload')
+            if payload.get('NextToken'):
+                next_token = payload.get('NextToken')
+                frappe.db.set_single_value('Amazon Seller Central Settings', 'next_token', next_token)
             if payload.get('Orders'):
-                orders = payload.get('Orders')[:5]
+                orders = payload.get('Orders')
                 for order in orders:
                     if order.get('AmazonOrderId'):
                         order_id = order.get('AmazonOrderId')
                         order_items = get_order_items(order_id)
-                        create_sales_invoices(docname, order, order_items)
+                        # get_financial_events(order_id)
+                        create_sales_invoice(order, order_items)
 
 @frappe.whitelist()
 def get_order_items(order_id):
@@ -127,56 +117,91 @@ def get_order_items(order_id):
     return []
 
 @frappe.whitelist()
-def create_sales_invoices(docname, order, order_items):
+def get_financial_events(order_id):
+    '''
+        Method to get order Items
+    '''
+    token = get_access_token()
+    api_base_url = "https://sellingpartnerapi-eu.amazon.com"
+    endpoint = f"{api_base_url}/finances/v0/orders/{order_id}/financialEvents"
+
+    headers = {
+        "x-amz-access-token": token,
+        "Content-Type": "application/json"
+    }
+
+    response = requests.get(endpoint, headers=headers)
+    response_json = response.json()
+    print("\n\n\n\n\n\n\n")
+    print("response_json : ", response_json)
+    if response.ok:
+        if response_json.get('payload'):
+            payload = response_json.get('payload')
+            return payload.get('FinancialEvents', [])
+    return []
+
+@frappe.whitelist()
+def create_sales_invoice(order, order_items):
     '''
     Method to Create Sales invoices.
     '''
-    amazon_payment_central_settings = frappe.get_doc('Amazon Seller Central Settings', docname)
     default_amazon_customer = frappe.db.get_single_value('eSeller Settings', 'default_amazon_customer')
     default_pos_profile = frappe.db.get_single_value('eSeller Settings', 'default_pos_profile')
+    create_items_if_not_exists = frappe.db.get_single_value("Amazon Seller Central Settings", "create_items_if_not_exists")
 
     if not default_amazon_customer:
         frappe.throw('Please configure the `Default Amazon Customer` in {0}'.format(get_link_to_form('eSeller Settings', 'eSeller Settings')))
     if not default_pos_profile:
         frappe.throw('Please configure the `Default POS Profile` in {0}'.format(get_link_to_form('eSeller Settings', 'eSeller Settings')))
 
-    sales_invoice_count = 0
+    new_sales_invoice = frappe.new_doc('Sales Invoice')
+    new_sales_invoice.custom_amazon_order_id = order.get('AmazonOrderId')
+    new_sales_invoice.custom_transaction_type = order.get('OrderType')
+    purchase_date = order.get('PurchaseDate')
+    if purchase_date:
+        posting_date = datetime.strptime(purchase_date, '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d')
+        new_sales_invoice.posting_date = posting_date
+    new_sales_invoice.customer = default_amazon_customer
     for item in order_items:
-        new_sales_invoice = frappe.new_doc('Sales Invoice')
-        new_sales_invoice.custom_amazon_order_id = order.get('AmazonOrderId')
-        new_sales_invoice.custom_transaction_type = order.get('OrderType')
-        purchase_date = order.get('PurchaseDate')
-        if purchase_date:
-	        posting_date = datetime.strptime(purchase_date, '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d')
-	        new_sales_invoice.posting_date = posting_date
-        new_sales_invoice.customer = default_amazon_customer
-        item_code = item.get('SellerSKU')
+        amazon_item_code = item.get('SellerSKU')
+        qty = item.get('QuantityOrdered') or 1
+        item_code = None
+        if frappe.db.exists('Item', { 'custom_amazon_item_code': amazon_item_code }):
+            item_code = frappe.db.get_value('Item', { 'custom_amazon_item_code': amazon_item_code })
+        else:
+            if create_items_if_not_exists:
+                default_item_group = frappe.db.get_single_value("Amazon Seller Central Settings", "default_item_group")
+                item_doc = frappe.new_doc('Item')
+                item_doc.item_code = amazon_item_code
+                item_doc.item_name = amazon_item_code 
+                item_doc.description = item.get('Title') or '' 
+                item_doc.item_group = default_item_group
+                item_doc.custom_amazon_item_code = amazon_item_code
+                item_doc.flags.ignore_mandatory = True
+                item_doc.flags.ignore_validate = True
+                item_doc.save()
+                item_code = item_doc.name
         item_price = item.get('ItemPrice', {}).get('Amount', 0)
-        new_sales_invoice.append('items', {
-            'item_code': item_code,
-            'qty': 1,
-            'rate': item_price,
-            'amount': item_price,
-            'custom_amazon_fees': item_price,
-            'allow_zero_valuation_rate': 1,
-            'income_account': 'Sales - E',
-        })
-        new_sales_invoice.update_stock = 1
-        new_sales_invoice.is_pos = 1
-        new_sales_invoice.pos_profile = default_pos_profile
-        new_sales_invoice.flags.ignore_mandatory = True
-        new_sales_invoice.flags.ignore_validate = True
-        new_sales_invoice.set_missing_values()
-        new_sales_invoice.calculate_taxes_and_totals()
-        new_sales_invoice.outstanding_amount = 0
-        new_sales_invoice.disable_rounded_total = 1
-        new_sales_invoice.save()
-        sales_invoice_count += 1
-
-        amazon_payment_central_settings.sales_invoice_created = 1
-        amazon_payment_central_settings.save()
-        frappe.msgprint(
-	        f"{sales_invoice_count} Sales Invoices Created.",
-	        indicator="green",
-	        alert=True,
-	    )
+        if item_code:
+            new_sales_invoice.append('items', {
+                'item_code': item_code,
+                'qty': qty,
+                'rate': item_price,
+                'amount': item_price,
+                'allow_zero_valuation_rate': 1
+            })
+    new_sales_invoice.update_stock = 1
+    new_sales_invoice.is_pos = 1
+    new_sales_invoice.pos_profile = default_pos_profile
+    new_sales_invoice.flags.ignore_mandatory = True
+    new_sales_invoice.flags.ignore_validate = True
+    new_sales_invoice.set_missing_values()
+    new_sales_invoice.calculate_taxes_and_totals()
+    new_sales_invoice.outstanding_amount = 0
+    new_sales_invoice.disable_rounded_total = 1
+    new_sales_invoice.save()
+    frappe.msgprint(
+        f"Sales Invoices {new_sales_invoice.name} Created.",
+        indicator="green",
+        alert=True,
+    )
