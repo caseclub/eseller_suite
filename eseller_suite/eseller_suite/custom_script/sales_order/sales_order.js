998dd83d3db2b63d27aaaa7b6ea49955c9c23d7b
caseclub <caseclub@erp.caseclub.com>
2025-10-16 21:34:17 +0000

    Update sales_order.js to enable full control for MFN orders

--- eseller_suite/eseller_suite/custom_script/sales_order/sales_order.js
+++ eseller_suite/eseller_suite/custom_script/sales_order/sales_order.js
@@ -1,6 +1,10 @@
+//After updating:
+//bench build
+//bench --site erp.caseclub.com clear-cache
+//bench restart
 frappe.ui.form.on('Sales Order', {
     refresh(frm) {
-        if (frm.doc.amazon_order_id) {
+        if (frm.doc.amazon_order_id && frm.doc.fulfillment_channel !== 'MFN') {
             frm.disable_form();
             frm.disable_save();
             $('.custom-actions').hide()
